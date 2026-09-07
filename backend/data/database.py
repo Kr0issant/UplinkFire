756cdef6974861720c36d530074417fb9996f496
Kr0issant <93475036+Kr0issant@users.noreply.github.com>
2026-09-07 19:09:50 +0530

    restructure backend directories

--- backend/data/database.py
+++ backend/data/database.py
@@ -57,6 +57,7 @@ class Database:
         conn = sqlite3.connect(DB_PATH)
         conn.row_factory = sqlite3.Row
         conn.execute("PRAGMA foreign_keys = ON;")
+        conn.execute("PRAGMA journal_mode = WAL;")
         return conn
 
     # === Settings ===
