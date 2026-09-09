3b1b099f5540631e995c8c68e0bd35c9dadda06b
Kr0issant <93475036+Kr0issant@users.noreply.github.com>
2026-09-09 14:03:03 +0530

    add job manager for tracking uploads/downloads

---
+++ backend/data/job_manager.py
@@ -0,0 +1,34 @@
+import asyncio, uuid
+from enum import Enum
+from typing import Dict, Tuple
+
+class JobType(Enum):
+    UPLOAD = 1
+    DOWNLOAD = 2
+
+class JobManager:
+    def __init__(self):
+        self._upload_jobs: Dict[str, asyncio.Queue] = {}
+        self._download_jobs: Dict[str, asyncio.Queue] = {}
+
+    def register_job(self, type: JobType) -> Tuple[str, asyncio.Queue]:
+        job_id = uuid.uuid4().hex
+        queue = asyncio.Queue()
+
+        match type:
+            case JobType.UPLOAD: self._upload_jobs[job_id] = queue
+            case JobType.DOWNLOAD: self._download_jobs[job_id] = queue
+
+        return (job_id, queue)
+
+    def get_queue(self, type: JobType, job_id: str) -> asyncio.Queue:
+        match type:
+            case JobType.UPLOAD: return self._upload_jobs.get(job_id)
+            case JobType.DOWNLOAD: return self._download_jobs.get(job_id)
+
+    def remove_job(self, type: JobType, job_id: str):
+        if type == JobType.UPLOAD and job_id in self._upload_jobs:
+            del self._upload_jobs[job_id]
+        elif type == JobType.DOWNLOAD and job_id in self._download_jobs:
+            del self._download_jobs[job_id]
+    
